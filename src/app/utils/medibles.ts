3d43ec48169d1fe2cfd872967ea86b8118e8ec3e
Neutron21 <tanerkratos@hotmail.com>
2024-11-27 13:29:43 -0600

    agregando json de documentos

--- src/app/utils/medibles.ts
+++ src/app/utils/medibles.ts
@@ -1,17 +1,62 @@
-export const medibles = {
+export const medibles = [
     // plazo
     // monto minimo
     // monto maximo
     // antiguedad de la empresa 
     // ingresos
     // edad
-    konfio : {
+
+    // KONFIO
+    {
+        tipoPersona: "PFAE",
+        financiera: "KONFIO",
+        tipo: "SIMPLE",
         plazoMin: 12,
         plazoMax: 24,
         montoMin: 80000,
         montoMax: 1000000,
-        antiguedadEmpresa: 24,
+        antiguedadEmpresa: 3,
+        ingresos: 80000,
+        edadMin:25,
+        edadMax:75
+    },
+    {
+        tipoPersona: "PM",
+        financiera: "KONFIO",
+        tipo: "SIMPLE",
+        plazoMin: 12,
+        plazoMax: 24,
+        montoMin: 80000,
+        montoMax: 1000000,
+        antiguedadEmpresa: 3,
+        ingresos: 100000,
+        edadMin:25,
+        edadMax:75
+    },
+    {
+        tipoPersona: "PFAE",
+        financiera: "KONFIO",
+        tipo: "TDC",
+        plazoMin: 0,
+        plazoMax: 0,
+        montoMin: 8000,
+        montoMax: 1000000,
+        antiguedadEmpresa: 6,
         ingresos: 2,
-        edad:25-75
+        edadMin:25,
+        edadMax:75
+    },
+    {
+        tipoPersona: "PFAE",
+        financiera: "KONFIO",
+        tipo: "TDC",
+        plazoMin: 0,
+        plazoMax: 0,
+        montoMin: 8000,
+        montoMax: 1000000,
+        antiguedadEmpresa: 6,
+        ingresos: "CONSTANTES",
+        edadMin:25,
+        edadMax:75
     }
-}
+]
