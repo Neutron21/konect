b0b3a15a68375a73eb46d2f6b0c3a9aba553e9c9
victorsilvaHR <victor.hugo.silva01@gmail.com>
2024-11-29 11:23:14 -0600

    se agregan productos a las imagenes y se quita el espacio en el formulario

--- src/app/utils/financieras.ts
+++ src/app/utils/financieras.ts
@@ -1,94 +1,105 @@
 export const financieras = [
-    {
-      id: "1",
-      nombre: "Konfio",
-      productos: [],
-      descripcion: "Compra de mercancía, cubrir deuda del negocio, pagos a proveedores.",
-      img: "assets/konfio.jpeg"
-    },
-    {
-      id: "2",
-      nombre: "PDN",
-      productos: [],
-      descripcion: "Para adquisición de inmueble productivo con uso de suelo comercial, oficinas o industrial.",
-      img: "assets/pdn.png"
-    },
-    {
-      id: "3",
-      nombre: "Covalto",
-      productos: [],
-      descripcion: "Para capital de trabajo, remodelación del negocio, crecimiento del negocio, compra de bienes de capital, inventarios, sustitución de pasivos.",
-      img: "assets/covalto.png"
-    },
-    {
-      id: "4",
-      nombre: "Anticipa",
-      productos: [],
-      descripcion: "Activos productivos / Transporte / Maquinaria / Cómputo / Equipo especializado / Mobiliario",
-      img: "assets/anticipa.png"
-    },
-    {
-      id: "5",
-      nombre: "BX+",
-      productos: [],
-      descripcion: "Activos productivos / Transporte / Maquinaria / Cómputo / Equipo especializado / Mobiliario",
-      img: "assets/bx+.png"
-    },
-    {
-      id: "6",
-      nombre: "ION",
-      productos: [],
-      descripcion: "Adquisición de inmuebles, Capital de trabajo, Financiamiento para expansión, financiamiento de proyectos de inversión productivos, Destino libre.",
-      img: "assets/Ion.png"
-    },
-    {
-      id: "7",
-      nombre: "HeyBanco",
-      productos: [],
-      descripcion: "Capital de Trabajo, Adquisición de activos fijos, proyectos de inversión, pago de pasivos.",
-      img: "assets/heybanco.jpg"
-    },
-    {
-      id: "8",
-      nombre: "ActiveLeasing",
-      productos: [],
-      descripcion: "Compra de activos productivos (bodegas industriales, oficinas, edificios, locales comerciales).",
-      img: "assets/activeleasing.jpeg"
-    },
-    {
-      id: "9",
-      nombre: "HayCash",
-      productos: [],
-      descripcion: "Capital de trabajo, Apertura de nuevas sucursales, Compra de activo fijo, Remodelación, Publicidad & marketing, y muchos más.",
-      img: "assets/haycash.png"
-    },
-    {
-      id: "10",
-      nombre: "Finsus",
-      productos: [],
-      descripcion: "Gobierno, giros negros, outsourcing nómina, combustibles y derivados, sindicatos, traslado de valores, sociedades de inversión, etc.",
-      img: "assets/finsus.png"
-    },
-    {
-      id: "11",
-      nombre: "FinBeABC",
-      productos: [],
-      descripcion: "Adquisición de activos fijos, Compra y/o remodelación de inmuebles, Construcción de bodegas y naves industriales, pago de pasivos.",
-      img: "assets/finbeabc.png"
-    },
-    {
-      id: "12",
-      nombre: "Xepelin",
-      productos: [],
-      descripcion: "Seguridad privada, despachos jurídicos y contables, outsourcing, aseguradoras, gobierno, minería, gasolinerías, sofom, casas de apuestas.",
-      img: "assets/xepelin.jpeg"
-    },
-    {
-      id: "13",
-      nombre: "Arrenda+",
-      productos: [],
-      descripcion: "Si es persona moral siempre se necesitará un aval persona física. En el caso de PFAE o PF solo se necesitará aval si el prospecto no cuenta con exposición de buró de crédito.",
-      img: "assets/arrenda+.webp"
-    }
-  ];
-  
+  {
+    id: "1",
+    nombre: "Konfio",
+    productos: ["Crédito Revolvente", "Crédito Simple (sin garantía)"],
+    descripcion: "Compra de mercancía, cubrir deuda del negocio, pagos a proveedores.",
+    img: "assets/konfio.jpeg"
+  },
+  {
+    id: "2",
+    nombre: "PDN",
+    productos: ["PDN EXPRESS (CRÉDITO SIMPLE)"],
+    descripcion: "Para adquisición de inmueble productivo con uso de suelo comercial, oficinas o industrial.",
+    img: "assets/pdn.png"
+  },
+  {
+    id: "3",
+    nombre: "Covalto",
+    productos: [
+      "CREDIACTIVO EXPRESS SIMPLE",
+      "CREDIACTIVO EXPRESS REVOLVENTE",
+      "CRÉDITO FACIL TPV (SIMPLE Y REVOLVENTE)",
+      "CRÉDITO FACIL FLUJOS (SIMPLE)",
+      "PROGRAMA IMPULSO + ESTADOS (SIMPLE)",
+      "CRÉDITO TOTAL (SIMPLE Y REVOLVENTE)",
+      "CRÉDITO INMUEBLES (SIMPLE)"
+    ],
+    descripcion: "Para capital de trabajo, remodelación del negocio, crecimiento del negocio, compra de bienes de capital, inventarios, sustitución de pasivos.",
+    img: "assets/covalto.png"
+  },
+  {
+    id: "4",
+    nombre: "Anticipa",
+    productos: ["ARRENDAMIENTO EQUIPO NUEVO", "ARRENDAMIENTO PURO Y FINANCIERO"],
+    descripcion: "Activos productivos / Transporte / Maquinaria / Cómputo / Equipo especializado / Mobiliario",
+    img: "assets/anticipa.png"
+  },
+  {
+    id: "5",
+    nombre: "BX+",
+    productos: ["ADQUISICION PYME (CRÉDITO SIMPLE)", "LIQUIDEZ PYME"],
+    descripcion: "Activos productivos / Transporte / Maquinaria / Cómputo / Equipo especializado / Mobiliario",
+    img: "assets/bx+.png"
+  },
+  {
+    id: "6",
+    nombre: "ION",
+    productos: ["INMOBILIARIO EMPRESARIAL (CRÉDITO SIMPLE)"],
+    descripcion: "Adquisición de inmuebles, Capital de trabajo, Financiamiento para expansión, financiamiento de proyectos de inversión productivos, Destino libre.",
+    img: "assets/Ion.png"
+  },
+  {
+    id: "7",
+    nombre: "HeyBanco",
+    productos: ["CREDITO SIMPLE", "CREDITO REVOLVENTE"],
+    descripcion: "Capital de Trabajo, Adquisición de activos fijos, proyectos de inversión, pago de pasivos.",
+    img: "assets/heybanco.jpg"
+  },
+  {
+    id: "8",
+    nombre: "ActiveLeasing",
+    productos: ["ARRENDAMIENTO S&LB"],
+    descripcion: "Compra de activos productivos (bodegas industriales, oficinas, edificios, locales comerciales).",
+    img: "assets/activeleasing.jpeg"
+  },
+  {
+    id: "9",
+    nombre: "HayCash",
+    productos: ["Crédito Sin Garantía", "Financiamiento con Terminal Punto de Venta (TPV)"],
+    descripcion: "Capital de trabajo, Apertura de nuevas sucursales, Compra de activo fijo, Remodelación, Publicidad & marketing, y muchos más.",
+    img: "assets/haycash.png"
+  },
+  {
+    id: "10",
+    nombre: "Finsus",
+    productos: ["FACTORAJE", "AGRO", "Crédito Paramétrico"],
+    descripcion: "Gobierno, giros negros, outsourcing nómina, combustibles y derivados, sindicatos, traslado de valores, sociedades de inversión, etc.",
+    img: "assets/finsus.png"
+  },
+  {
+    id: "11",
+    nombre: "FinBeABC",
+    productos: [
+      "ARRENDAMIENTO PURO",
+      "Crédito Simple (sin garantía)",
+      "Crédito Revolvente"
+    ],
+    descripcion: "Adquisición de activos fijos, Compra y/o remodelación de inmuebles, Construcción de bodegas y naves industriales, pago de pasivos.",
+    img: "assets/finbeabc.png"
+  },
+  {
+    id: "12",
+    nombre: "Xepelin",
+    productos: ["ANTICIPO DE VENTAS FUTURAS CON TARJETAS BANCARIAS"],
+    descripcion: "Seguridad privada, despachos jurídicos y contables, outsourcing, aseguradoras, gobierno, minería, gasolinerías, sofom, casas de apuestas.",
+    img: "assets/xepelin.jpeg"
+  },
+  {
+    id: "13",
+    nombre: "Arrenda+",
+    productos: ["Arrendamiento Puro", "Arrendamiento S&LB"],
+    descripcion: "Si es persona moral siempre se necesitará un aval persona física. En el caso de PFAE o PF solo se necesitará aval si el prospecto no cuenta con exposición de buró de crédito.",
+    img: "assets/arrenda+.webp"
+  }
+];
