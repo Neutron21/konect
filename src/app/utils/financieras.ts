72275cecfbbe65fd6fe4734cd69496f5c8301e04
Neutron21 <tanerkratos@hotmail.com>
2024-12-05 13:27:46 -0600

    agregando fases y proceso en json de financieras

--- src/app/utils/financieras.ts
+++ src/app/utils/financieras.ts
@@ -2,6 +2,8 @@ export const financieras = [
   {
     id: "1",
     nombre: "Konfio",
+    proceso:"link",
+    fase:"1",
     productos: ["Crédito Revolvente", "Crédito Simple (sin garantía)"],
     descripcion: "Compra de mercancía, cubrir deuda del negocio, pagos a proveedores.",
     img: "assets/konfio.jpeg"
@@ -9,6 +11,8 @@ export const financieras = [
   {
     id: "2",
     nombre: "PDN",
+    proceso:"link",
+    fase:"2",
     productos: ["PDN EXPRESS (CRÉDITO SIMPLE)"],
     descripcion: "Para adquisición de inmueble productivo con uso de suelo comercial, oficinas o industrial.",
     img: "assets/pdn.png"
@@ -16,6 +20,8 @@ export const financieras = [
   {
     id: "3",
     nombre: "Covalto",
+    proceso:"mail",
+    fase:"1",
     productos: [
       "CREDIACTIVO EXPRESS SIMPLE",
       "CREDIACTIVO EXPRESS REVOLVENTE",
@@ -31,6 +37,8 @@ export const financieras = [
   {
     id: "4",
     nombre: "Anticipa",
+    proceso:"link",
+    fase:"2",
     productos: ["ARRENDAMIENTO EQUIPO NUEVO", "ARRENDAMIENTO PURO Y FINANCIERO"],
     descripcion: "Activos productivos / Transporte / Maquinaria / Cómputo / Equipo especializado / Mobiliario",
     img: "assets/anticipa.png"
@@ -38,6 +46,8 @@ export const financieras = [
   {
     id: "5",
     nombre: "BX+",
+    proceso:"mail",
+    fase:"2",
     productos: ["ADQUISICION PYME (CRÉDITO SIMPLE)", "LIQUIDEZ PYME"],
     descripcion: "Activos productivos / Transporte / Maquinaria / Cómputo / Equipo especializado / Mobiliario",
     img: "assets/bx+.png"
@@ -45,6 +55,8 @@ export const financieras = [
   {
     id: "6",
     nombre: "ION",
+    proceso:"mail",
+    fase:"2",
     productos: ["INMOBILIARIO EMPRESARIAL (CRÉDITO SIMPLE)"],
     descripcion: "Adquisición de inmuebles, Capital de trabajo, Financiamiento para expansión, financiamiento de proyectos de inversión productivos, Destino libre.",
     img: "assets/Ion.png"
@@ -52,6 +64,8 @@ export const financieras = [
   {
     id: "7",
     nombre: "HeyBanco",
+    proceso:"mail",
+    fase:"1",
     productos: ["CREDITO SIMPLE", "CREDITO REVOLVENTE"],
     descripcion: "Capital de Trabajo, Adquisición de activos fijos, proyectos de inversión, pago de pasivos.",
     img: "assets/heybanco.jpg"
@@ -59,6 +73,8 @@ export const financieras = [
   {
     id: "8",
     nombre: "ActiveLeasing",
+    proceso:"mail",
+    fase:"1",
     productos: ["ARRENDAMIENTO S&LB"],
     descripcion: "Compra de activos productivos (bodegas industriales, oficinas, edificios, locales comerciales).",
     img: "assets/activeleasing.jpeg"
@@ -66,6 +82,8 @@ export const financieras = [
   {
     id: "9",
     nombre: "HayCash",
+    proceso:"link",
+    fase:"1",
     productos: ["Crédito Sin Garantía", "Financiamiento con Terminal Punto de Venta (TPV)"],
     descripcion: "Capital de trabajo, Apertura de nuevas sucursales, Compra de activo fijo, Remodelación, Publicidad & marketing, y muchos más.",
     img: "assets/haycash.png"
@@ -73,6 +91,8 @@ export const financieras = [
   {
     id: "10",
     nombre: "Finsus",
+    proceso:"link",
+    fase:"2",
     productos: ["FACTORAJE", "AGRO", "Crédito Paramétrico"],
     descripcion: "Gobierno, giros negros, outsourcing nómina, combustibles y derivados, sindicatos, traslado de valores, sociedades de inversión, etc.",
     img: "assets/finsus.png"
@@ -80,6 +100,8 @@ export const financieras = [
   {
     id: "11",
     nombre: "FinBeABC",
+    proceso:"mail",
+    fase:"2",
     productos: [
       "ARRENDAMIENTO PURO",
       "Crédito Simple (sin garantía)",
@@ -91,6 +113,8 @@ export const financieras = [
   {
     id: "12",
     nombre: "Xepelin",
+    proceso:"link",
+    fase:"1",
     productos: ["ANTICIPO DE VENTAS FUTURAS CON TARJETAS BANCARIAS"],
     descripcion: "Seguridad privada, despachos jurídicos y contables, outsourcing, aseguradoras, gobierno, minería, gasolinerías, sofom, casas de apuestas.",
     img: "assets/xepelin.jpeg"
@@ -98,6 +122,8 @@ export const financieras = [
   {
     id: "13",
     nombre: "Arrenda+",
+    proceso:"mail",
+    fase:"1",
     productos: ["Arrendamiento Puro", "Arrendamiento S&LB"],
     descripcion: "Si es persona moral siempre se necesitará un aval persona física. En el caso de PFAE o PF solo se necesitará aval si el prospecto no cuenta con exposición de buró de crédito.",
     img: "assets/arrenda+.webp"
